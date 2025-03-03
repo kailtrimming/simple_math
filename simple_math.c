b2959519ca73de3ec25be10063f867e515f83c5d
kailtrimming <kgtrimming@gmail.com>
2025-03-03 15:39:01 +0100

    first version of simple math - no test

---
+++ simple_math.c
@@ -0,0 +1,19 @@
+/** \file simple_math.c
+ *  \brief a toy example of math library 
+ *  \author Kail Trimming
+ 
+ */
+
+#include "simple_math.h"
+
+float mean(int a, int b){
+    return (a+b)/2;
+}
+
+int max (int a, int b) {
+		
+	if (a > b )
+		return a;
+	return b;
+	
+}
---
+++ simple_math.h
@@ -0,0 +1,13 @@
+/** \file simple_math.h
+    \brief a toy example of math library
+	\author Kail Trimming
+	
+*/
+
+#ifndef _SIMPLEMATH_
+#define _SIMPLEMATH_
+
+float mean(int a, int b);
+int max ( a > b );
+
+#endif
