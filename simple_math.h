f683e881c7cb0b2c38fd0c826eedfd7b36edc0ff
kailtrimming <kgtrimming@gmail.com>
2025-03-04 12:16:41 +0100

    with complex numbers - tested

--- simple_math.h
+++ simple_math.h
@@ -7,7 +7,19 @@
 #ifndef _SIMPLEMATH_
 #define _SIMPLEMATH_
 
+typedef struct complex_int {
+	int re;
+	int imm;
+} cx_int_t;
+
+typedef struct complex_float {
+	float re;
+	float imm;
+} cx_float_t;
+
 float mean(int a, int b);
+cx_float_t cx_mean (cx_int_t a, cx_int_t b);
+
 int max (int a, int b);
 
 #endif
